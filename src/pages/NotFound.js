21f857a24dd1564a1880d8334ea4bd68e2b70092
returnys <return.ys@gmail.com>
2023-06-27 18:08:53 +0900

    update: NotFound 영역 업데이트

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -10,7 +10,7 @@ const NotFound = () => {
           Page not found
         </h1>
         <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry, we couldn’t find the page you’re looking for.
+          Sorry, we couldn’t find the page you’re looking for. Check URI.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
